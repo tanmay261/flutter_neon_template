d1725d496737118d9f39271a86a79efd653eed90
Tanmay Agrawal <tanmaytanu.10@gmail.com>
2024-08-25 21:54:14 +0530

    chore: auth_screens_setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,24 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_neon_template/screens/home_screen.dart';
+import 'package:flutter_neon_template/themes/theme.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const NotMyApp());
+}
+
+class NotMyApp extends StatelessWidget {
+  const NotMyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: lightMode,
+      home: const WelcomeScreen(),
+    );
+  }
 }
 
 class MyApp extends StatelessWidget {
