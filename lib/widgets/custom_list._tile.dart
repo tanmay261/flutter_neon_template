d74bc2aa243cdefbb395fea765e2186a2d1427c7
Tanmay Agrawal <tanmaytanu.10@gmail.com>
2024-08-26 22:07:27 +0530

    chore: graphs_added

---
+++ lib/widgets/custom_list._tile.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class CustomListTile {
+  final IconData icon;
+  final String title;
+  CustomListTile({
+    required this.icon,
+    required this.title,
+  });
+}
